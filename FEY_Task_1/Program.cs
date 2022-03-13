49eed750eb4c146df8e3a9be2a0017158dd73296
Evgeniy Fedotow <koronazheka@outlook.com>
2022-03-13 15:27:15 +0300

    Completed first week's task 1 and 2

---
+++ FEY_Task_1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace FEY_Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int crystalPrice = 50;
+            int playerBalance;
+            int desiredCrystals;
+            int crystalCount = 0;
+            Console.WriteLine("How many gold do you have?");
+            if (!int.TryParse(Console.ReadLine(), out playerBalance))
+            {
+                Console.WriteLine("Balance input is not correct");
+                Console.ReadKey();
+                return;
+            };
+            Console.WriteLine("How many crystals would you like to buy");
+            if (!int.TryParse(Console.ReadLine(), out desiredCrystals))
+            {
+                Console.WriteLine("Crystal count input is not correct");
+                Console.ReadKey();
+                return;
+            };
+            while(crystalCount < desiredCrystals && playerBalance - crystalPrice>=0 )
+            {
+                playerBalance -= crystalPrice;
+                crystalCount++;
+            }
+            Console.WriteLine($"You had bought {crystalCount} crystals");
+            Console.WriteLine($"Gold left: {playerBalance}");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ FEY_Task_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FEY_Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+            Console.WriteLine("Write \"exit\" to stop loop");
+            while(input!= "exit")
+            {
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
